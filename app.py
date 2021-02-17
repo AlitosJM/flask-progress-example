56bb87c1056046d2c92cebcfdf7c5c75c710fa36
AliTosJM <alitosjm@gmail.com>
2021-02-16 23:32:22 -0600

    Revisar html

--- app.py
+++ app.py
@@ -3,21 +3,29 @@ import time
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/python', methods=['GET', 'POST'])
+def msg():
+    return "hola"
+
 
 @app.route('/progress')
 def progress():
-	def generate():
-		x = 0
-		
-		while x <= 100:
-			yield "data:" + str(x) + "\n\n"
-			x = x + 10
-			time.sleep(0.5)
+    def generate():
+        x = 0
 
-	return Response(generate(), mimetype= 'text/event-stream')
+        while x <= 100:
+            yield "data:" + str(x) + "\n\n"
+            x = x + 10
+            time.sleep(0.5)
+
+    return Response(generate(), mimetype='text/event-stream')
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
